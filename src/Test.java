551aae0ba9e841aa40820753174ae5cf2be0593a
dlfdyd96 <1yongs_@naver.com>
2021-10-24 20:47:19 +0900

    FEAT: cafe24, nhn

--- src/Test.java
+++ src/Test.java
@@ -4,8 +4,9 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.util.*;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.stream.Collectors;
@@ -13,6 +14,55 @@ import java.util.stream.Stream;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(4 % 6);
+        int ans = new Solution().solution(new int[]{1, 2, 5, 7, 10, 15, 18, 20}, 15);
+        System.out.println(ans);
     }
+}
+
+class Solution {
+    // 크루스칼 알고리즘은 각 도시를 도로를 이용해 연결하고자 할 때 비용을 최소한으로 하고자 할 때 실제로 적용되는 알고리즘.
+    List<Node> edges = new ArrayList<>();
+
+    boolean find(int set[], int a, int b) {
+        a = getParent(set, a);
+        b = getParent(set, b);
+
+        if (a == b) return true;
+        return false;
+    }
+
+    private int getParent(int[] set, int a) {
+        if (set[a] == a) return a;
+        return set[a] = getParent(set, set[a]);
+    }
+
+    private void unionParent(int set[], int a, int b) {
+        a = getParent(set, a);
+        b = getParent(set, b);
+
+        if (a < b) set[b] = a;
+        else
+            set[a] = b;
+    }
+
+    public void solution(int n) {
+        int[] set = new int[n];
+        for (int i = 0; i < n; i++) {
+            set[i] = i;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < edges.size(); i++) {
+            if (!find(set, edges.get(i).start - 1, edges.get(i).end - 1)) {
+                sum += edges.get(i).distance;
+                unionParent(set, edges.get(i).start - 1, edges.get(i).end - 1);
+            }
+        }
+    }
+}
+
+class Node {
+    public int start;
+    public int end;
+    public int distance;
 }
---
+++ src/hnhgroup/p1/Main.java
@@ -0,0 +1,23 @@
+package hnhgroup.p1;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> test = new ArrayList<>();
+        test.remove(1);
+    }
+    public void removeEl(int target, List<Integer> ar) {
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        Map<Integer, Node> m = new HashMap<>();
+        Set<Integer> integers = m.keySet();
+        integers.
+    }
+
+}
+
+class Node {
+    public Set<Integer> kids;
+}
